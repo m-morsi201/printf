f7131506ef618dc46035ddc2b93e63eaa8f64e2f
BasantMansour <basantmansour475@gmail.com>
2023-08-22 18:22:17 -0700

    handel

---
+++ _handel.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _handel - handel function
+ * @str: this is String
+ * @list: this is List of arguments
+ * Return: arguments and string
+ **/
+int _handel(const char *str, va_list list)
+{
+	int s, n, x;
+
+	s = 0;
+	for (n = 0; str[n] != 0; n++)
+	{
+		if (str[n] == '%')
+		{
+			x = percent_handler(str, list, &n);
+			if (x == -1)
+				return (-1);
+
+			s += x;
+			continue;
+		}
+
+		putchar(str[n]);
+		s = s + 1;
+	}
+
+
+	return (s);
+}
