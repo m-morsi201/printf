f9019c201e39832bc7a12b7abca1a0e1f90a6a61
m-morsi201 <muhammadmorsy5@gmail.com>
2023-08-25 16:23:58 -0700

    edit main.h

--- main.h
+++ main.h
@@ -10,8 +10,14 @@
 int _strlen(const char *);
 int _printf(const char *, ...);
 int _printf(const char *format, ...);
-int handel(const char *, va_list);
+int _handel(const char *, va_list);
 int per_handel(const char *, va_list, int *);
+
+/**
+ * struct _format - it is a data type struct.
+ * @type: it is a Format.
+ * @f: it is a function.
+*/
 
 typedef struct _format
 {
